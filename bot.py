7380d782ce199ec1ed08f45d8a3e924cd482e14c
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2020-10-07 12:00:42 -0700

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
     print('I was created in ' + birth_year + '.')
 
-
+ 
 def remind_name():
     print('Please, remind me your name.')
     name = input()
